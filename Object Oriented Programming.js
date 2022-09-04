d456d54f7296b52bf32ffee7539c7113da061525
hajjishirwac <shirwachajji@gmail.com>
2022-09-04 09:17:16 +0300

    Iterate Over All Properties

--- Object Oriented Programming.js
+++ Object Oriented Programming.js
@@ -81,3 +81,24 @@ function Dog(name) {
   
   
   let beagle = new Dog("Snoopy");
+//   Iterating Over All Properties
+
+// function Dog(name) {
+    //   this.name = name;
+    // }
+    
+    // Dog.prototype.numLegs = 4;
+    
+    // let beagle = new Dog("Snoopy");
+    
+    // let ownProps = [];
+    // let prototypeProps = [];
+    
+    
+    // for (let property in beagle) {
+    //   if(Dog.hasOwnProperty(property)) {
+    //     ownProps.push(property);
+    //   } else {
+    //     prototypeProps.push(property);
+    //   }
+    // }
