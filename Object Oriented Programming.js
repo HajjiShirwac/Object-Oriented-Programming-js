946ed09e1522b02cc22d17f540c34eb43d3ee46e
hajjishirwac <shirwachajji@gmail.com>
2022-09-04 10:46:47 +0300

    Change the Prototype to a New Object

--- Object Oriented Programming.js
+++ Object Oriented Programming.js
@@ -116,3 +116,19 @@ function Dog(name) {
           return false
         }
       }
+
+    //   Change the Prototype to a New Object
+    function Dog(name) {
+        this.name = name;
+      }
+      
+      Dog.prototype = {
+        
+          numLegs: 2, 
+        eat: function() {
+          console.log("nom nom nom");
+        },
+        describe: function() {
+          console.log("My name is " + this.name);
+        }
+      };
