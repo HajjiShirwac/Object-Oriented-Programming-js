87067f2ad1e1cac3e22de0ebf13924eede75de94
hajjishirwac <shirwachajji@gmail.com>
2022-09-04 09:04:14 +0300

    Understand Own Properties

--- Object Oriented Programming.js
+++ Object Oriented Programming.js
@@ -59,3 +59,17 @@ let House = function (numBedrooms) {
   // Only change code below this line
   let myHouse = new House(14);
   myHouse instanceof House;
+// Understanding Own Properties
+function Bird(name) {
+    this.name = name;
+    this.numLegs = 2;
+  }
+  
+  let canary = new Bird("Tweety");
+  let ownProps = [];
+  // Only change code below this line
+  for (let property in canary) {
+    if (canary.hasOwnProperty(property)) {
+      ownProps.push(property);
+    }
+  }
