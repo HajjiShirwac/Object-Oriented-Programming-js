3dffe89066ac83ddbf462fd84b46ef4b1f35857d
hajjishirwac <shirwachajji@gmail.com>
2022-09-05 15:01:10 +0300

    Override Inherited Methods

--- Object Oriented Programming.js
+++ Object Oriented Programming.js
@@ -268,3 +268,24 @@ Dog.prototype.bark = function() {
 // let beagle = new Dog();
 // beagle.eat();
 // beagle.bark();
+
+/////Override Inherited Methods
+
+function Bird() { }
+
+Bird.prototype.fly = function() { return "I am flying!"; };
+
+function Penguin() { }
+Penguin.prototype = Object.create(Bird.prototype);
+Penguin.prototype.constructor = Penguin;
+
+// Only change code below this line
+
+Penguin.prototype.fly = function() {
+  return "Alas, this is a flightless bird.";
+};
+
+// Only change code above this line
+
+let penguin = new Penguin();
+console.log(penguin.fly());
