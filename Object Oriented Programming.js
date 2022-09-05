bf3e073912ffbef23883e0d09472cf07376d7e4b
hajjishirwac <shirwachajji@gmail.com>
2022-09-05 16:00:24 +0300

    Use an IIFE to Create a Module

--- Object Oriented Programming.js
+++ Object Oriented Programming.js
@@ -325,3 +325,21 @@ let bird = {
 (function () {
     console.log("A cozy nest is ready");
   })()
+
+  //Use an IIFE to Create a Module
+
+  let funModule = ( function () {
+    return {
+       isCuteMixin : function(obj) {
+    obj.isCute = function() {
+      return true;
+    };
+  
+    },
+    singMixin : function(obj) {
+    obj.sing = function() {
+      console.log("Singing to an awesome tune");
+    };
+    }
+    };
+  })();
